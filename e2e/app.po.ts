7fe1af8557c70f21ea04ef808c2204b884ae28ce
Fineas Muresan <muresan.it@gmail.com>
2019-02-22 23:31:55 +0200

    feat(core): update tests
    - correct typo
    
    #5

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root > div > mat-toolbar.mat-primary > span')).getText();
   }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
@@ -9,8 +16,15 @@ import { WeatherServiceFake } from './weather/weather.service.fake';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [AppComponent, CurrentWeatherComponent],
+      imports: [HttpClientTestingModule, MatToolbarModule],
+      declarations: [
+        AppComponent,
+        CurrentWeatherComponent,
+        MatCardTitle,
+        MatCardHeader,
+        MatCardContent,
+        MatCard,
+      ],
       providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
     }).compileComponents();
   }));
@@ -21,10 +35,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a span tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('LocalCast Weather');
+    expect(compiled.querySelector('span').textContent).toContain('LocalCast Weather');
   }));
 });
